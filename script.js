99cdaed726b85c4ecd5e38345d5c2f18a2688438
Maninani93 <javvadinani422@gmail.com>
2025-03-03 01:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded", function () {
+    let enterButton = document.getElementById("enterBtn");
+    let statusParagraph = document.getElementById("status");
+
+    enterButton.addEventListener("click", function () {
+        statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+    });
+});
